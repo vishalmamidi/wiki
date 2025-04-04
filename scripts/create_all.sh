c17a5f27820f8ad3773a2f6e40a851786bfc95e6
Vishal Mamidi <mamidivishalsa@gmail.com>
2025-04-04 17:56:01 +0000

    update path

--- scripts/create_all.sh
+++ scripts/create_all.sh
@@ -6,13 +6,10 @@ OUTPUT_FILE="All.md"
 # Navigate one folder back
 cd ..
 
-# Remove the output file if it already exists
-if [ -f "$OUTPUT_FILE" ]; then
-    rm "$OUTPUT_FILE"
-fi
-
+# Override the output file instead of removing it
 # Find all .md files one level above the current directory
 # Add the file name as a title before concatenating its content into the output file
+: > "$OUTPUT_FILE" # Truncate or create the output file
 find ../ -type f -name "*.md" | while read -r file; do
     echo "# $(basename "$file")" >> "$OUTPUT_FILE"
     cat "$file" >> "$OUTPUT_FILE"
