76ffda8d29e5b32d809f562520c14b20e00db341
Vishal Mamidi <mamidivishalsa@gmail.com>
2025-04-04 18:27:20 +0000

    clean up

--- scripts/create_all.sh
+++ scripts/create_all.sh
@@ -3,13 +3,6 @@
 # Define the output file
 OUTPUT_FILE="All.md"
 
-# Print the present working directory
-echo "Starting working directory: $(pwd)"
-
-
-# Print the present working directory
-echo "Current working directory: $(pwd)"
-
 
 : > "$OUTPUT_FILE" # Truncate or create the output file
 find ../ -type f -name "*.md" | while read -r file; do
